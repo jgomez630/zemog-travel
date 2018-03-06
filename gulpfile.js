466425ee932ee10a5cccf529f6c20de993028fe5
Jeff <jgomez630@aol.com>
2018-03-05 21:52:09 -0800

    3rd commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default',function(){
   console.log('Here is your default task.');
@@ -22,12 +23,24 @@ gulp.task('styles',function(){
 
 gulp.task('watch',function(){
 
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html',function(){
-    gulp.start('html'); //starts html task
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
 });
+
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+});
+
